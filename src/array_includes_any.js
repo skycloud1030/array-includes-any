99d1aa1046d08430318a5cff0133b1a39bc698ba
skycloud1030 <skycloud1030@gmail.com>
2018-05-18 10:58:07 +0800

    Update fbjs/lib/shallowEqual.js

--- src/array_includes_any.js
+++ src/array_includes_any.js
@@ -1,5 +1,5 @@
 /*array_includes_any.js*/
-export function array_includes_any(array1, array2, type = "any") {
+export default function array_includes_any(array1, array2, type = "any") {
   if (type === "equal") {
     return shallowEqual(array1, array2);
   }
@@ -14,14 +14,14 @@ export function array_includes_any(array1, array2, type = "any") {
   ) {
     return false;
   }
-  // gen hash table
-  let objA = {};
+  // dictionary O(n)
+  let dictionary = {};
   for (let i = 0; i < array1.length; i++) {
-    objA[array1[i]] = array1[i];
+    dictionary[array1[i]] = array1[i];
   }
   // compare state
   for (let i = 0; i < array2.length; i++) {
-    if (objA.hasOwnProperty(array2[i])) {
+    if (dictionary.hasOwnProperty(array2[i])) {
       return true;
     }
   }
@@ -34,20 +34,36 @@ export function array_includes_any(array1, array2, type = "any") {
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @providesModule shallowEqual
  * @typechecks
- * @flow
+ *
  */
-const hasOwn = Object.prototype.hasOwnProperty;
 
+/*eslint-disable no-self-compare */
+
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+/**
+ * inlined Object.is polyfill to avoid requiring consumers ship their own
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is
+ */
 function is(x, y) {
+  // SameValue algorithm
   if (x === y) {
+    // Steps 1-5, 7-10
+    // Steps 6.b-6.e: +0 != -0
+    // Added the nonzero y check to make Flow happy, but it is redundant
     return x !== 0 || y !== 0 || 1 / x === 1 / y;
   } else {
+    // Step 6.a: NaN == NaN
     return x !== x && y !== y;
   }
 }
 
+/**
+ * Performs equality by iterating through keys on an object and returning false
+ * when any key has values which are not strictly equal between the arguments.
+ * Returns true when the values of all keys are strictly equal.
+ */
 function shallowEqual(objA, objB) {
   if (is(objA, objB)) return true;
 
@@ -65,12 +81,15 @@ function shallowEqual(objA, objB) {
 
   if (keysA.length !== keysB.length) return false;
 
-  for (let i = 0; i < keysA.length; i++) {
-    if (!hasOwn.call(objB, keysA[i]) || !is(objA[keysA[i]], objB[keysA[i]])) {
+  // Test for A's keys different from B.
+  for (var i = 0; i < keysA.length; i++) {
+    if (
+      !hasOwnProperty.call(objB, keysA[i]) ||
+      !is(objA[keysA[i]], objB[keysA[i]])
+    ) {
       return false;
     }
   }
 
   return true;
 }
-export default array_includes_any;
